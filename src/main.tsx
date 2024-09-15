27c0019d50a2c3a6e6d507ab629a2b816ab8b50f
Philipp Bönsch <philipp.boensch@softgames.de>
2024-09-15 14:12:56 +0200

    fix container not found

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import './index.css'
 import Routes from './routes'
 import { createRoot } from 'react-dom/client'
 
-const container = document.getElementById('app')
+const container = document.getElementById('root')
 if (!container) {
   throw new Error('Container not found')
 }
